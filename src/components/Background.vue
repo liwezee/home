2abadd3a1e57e15113dbb8dda8b08445ba357d40
liwezee <63093684+liwezee@users.noreply.github.com>
2025-05-18 20:53:16 +0800

    Update Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -39,13 +39,13 @@ const bgRandom = Math.floor(Math.random() * 10 + 1);
 // 更换壁纸链接
 const changeBg = (type) => {
   if (type == 0) {
-    bgUrl.value = `/images/background${bgRandom}.jpg`;
+    bgUrl.value = "https://t.alcy.cc/fj";
   } else if (type == 1) {
-    bgUrl.value = "https://api.dujin.org/bing/1920.php";
+    bgUrl.value = "https://t.alcy.cc/fj";
   } else if (type == 2) {
-    bgUrl.value = "https://api.vvhan.com/api/wallpaper/views";
+    bgUrl.value = "https://t.alcy.cc/fj";
   } else if (type == 3) {
-    bgUrl.value = "https://api.vvhan.com/api/wallpaper/acg";
+    bgUrl.value = "https://t.alcy.cc/fj";
   }
 };
 
@@ -76,7 +76,7 @@ const imgLoadError = () => {
       fill: "#efefef",
     }),
   });
-  bgUrl.value = `/images/background${bgRandom}.jpg`;
+  bgUrl.value = "https://t.alcy.cc/fj";
 };
 
 // 监听壁纸切换
